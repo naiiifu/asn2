45967fe933b7c21fe340afd0925b864f1c9046be
naiiifu <91281950+naiiifu@users.noreply.github.com>
2023-06-13 23:51:37 -0700

    Update studentController.java

--- src/main/java/com/example/demo/controllers/studentController.java
+++ src/main/java/com/example/demo/controllers/studentController.java
@@ -20,7 +20,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 
-@RestController
+@Controller
 public class studentController {  
     @Autowired
     private studentRepository studentRepo;
